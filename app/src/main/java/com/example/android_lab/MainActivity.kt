954ccfb27726dd3f85fc19bdf9591509f4492b42
Lamenut <peopie8862@mail.ru>
2025-09-17 01:00:43 +0700

    Working on lab2

--- app/src/main/java/com/example/android_lab/MainActivity.kt
+++ app/src/main/java/com/example/android_lab/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.android_lab
 
 import android.os.Bundle
+import android.widget.SeekBar
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.Date
 
+
+class RegInfo {
+    var fullName: String
+    var sex: Int
+    var course: Int
+    var difficulty: Int
+    var date: Date
+
+    constructor(_name: String, _sex: Int, _course: Int, _diff: Int, _date: Date) {
+        fullName = _name
+        sex = _sex
+        course = _course
+        difficulty = _diff
+        date = _date
+    }
+}
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,5 +34,7 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val seek: SeekBar = findViewById(R.id.sbDifficulty)
     }
 }
